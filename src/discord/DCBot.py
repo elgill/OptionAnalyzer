268ff96eb56132522eab2beb0126c7c2aa286ea5
Chris Gillin <cgillin2@gmail.com>
2022-01-12 23:50:51 -0500

    Added first discord bot code

---
+++ src/discord/DCBot.py
@@ -0,0 +1,18 @@
+import os
+import random
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='stock', help='Stonks?')
+async def cmd_stock(ctx):
+
+    response = "Stonks"
+    await ctx.send(response)
+
+bot.run(TOKEN)
