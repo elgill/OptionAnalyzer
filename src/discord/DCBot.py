91420cb888dab8b512d73ad51d74de7db7b4baa0
Chris Gillin <cgillin2@gmail.com>
2022-01-15 23:08:34 -0500

    fixed an oops

--- src/discord/DCBot.py
+++ src/discord/DCBot.py
@@ -12,7 +12,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='stock', help='Stonks?')
 async def cmd_stock(ctx,arg):
     arg=str(arg)
-    response = "Stonks: {arg}"
+    response = f"Stonks: {arg}"
     await ctx.send(response)
 
 bot.run(TOKEN)
