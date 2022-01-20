c4ed04f61f62e9b92ca945cfe22323a0287378c7
Chris Gillin <cgillin2@gmail.com>
2022-01-19 21:17:23 -0500

    Added news to discord bot return

--- src/DCBot.py
+++ src/DCBot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import datetime
 
 
 from discord.ext import commands
@@ -15,11 +16,20 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='stock', help='Stonks?')
 async def cmd_stock(ctx,arg):
+    await ctx.send("Let me get that for you..")
     arg=str(arg)
     ticker=api.getTicker(arg)
     tickerInfo=api.getInfo(ticker)
     response = f"Stonks: {arg}\n"
     response+= f"Market Price: {tickerInfo['regularMarketPrice']}"
     await ctx.send(response)
+    response=""
+    for article in api.getNews(ticker):
+        response=f"`{article['title']}`"
+        response+=f"\n{article['link']}"
+        response+=f"\n{datetime.datetime.fromtimestamp(article['providerPublishTime'])}"
+        
+        await ctx.send(response)
+    #await ctx.send(response)
 
 bot.run(TOKEN)
