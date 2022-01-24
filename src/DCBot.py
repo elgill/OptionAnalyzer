45178d90f68125bcc670976f6e15567ad59b535c
Chris Gillin <cgillin2@gmail.com>
2022-01-23 22:11:12 -0500

    Fixed my mess up

--- src/DCBot.py
+++ src/DCBot.py
@@ -14,9 +14,31 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+@bot.command(name='stock', help='Stonks?')
+async def cmd_stock(ctx,arg):
+    await ctx.send("Let me get that for you..")
+    arg=str(arg)
+    ticker=api.getTicker(arg)
+    tickerInfo=api.getInfo(ticker)
+    response = f"Stonks: {arg}\n"
+    response+= f"Market Price: {tickerInfo['regularMarketPrice']}"
+    await ctx.send(response)
+    response=""
+    maxArticles=4
+    cnt=1
+    for article in api.getNews(ticker):
+        cnt+=1
+        if (cnt>maxArticles):
+            break
+        response=f"`{article['title']}`"
+        response+=f"\n{article['link']}"
+        response+=f"\n{datetime.datetime.fromtimestamp(article['providerPublishTime'])}"
+        
+        await ctx.send(response)
+
 #arg2 can serve as optionname
 @bot.command(name='stock', help='Stonks?')
-async def cmd_stock(ctx,arg,arg2):
+async def cmd_stock_2_args(ctx,arg,arg2):
     await ctx.send("Let me get that for you..")
     arg=str(arg)
     ticker=api.getTicker(arg)
@@ -39,7 +61,7 @@ async def cmd_stock(ctx,arg,arg2):
     await ctx.send(str(arg2))
 
 @bot.command(name='stop', help='Stops?')
-async def cmd_stop(ctx,arg,arg2):
+async def cmd_stop(ctx):
     await ctx.send("Trying to stop")
     exit()
     await ctx.send("probs didn't work lol")
